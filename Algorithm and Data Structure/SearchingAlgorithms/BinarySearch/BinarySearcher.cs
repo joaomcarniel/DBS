0dc02831207b89bf7a4ce5de1b4c637cbc0da740
Joao Marcos Carniel <joaomarcoscrr@gmail.com>
2025-11-29 00:05:18 +0000

    Correcting binary search

---
+++ Algorithm and Data Structure/SearchingAlgorithms/BinarySearch/BinarySearcher.cs
@@ -0,0 +1,27 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearcher
+    {
+        public int GetKeyElement(int[] numList, int keyElement)
+        {
+            int low = 0;
+            int high = numList.Length - 1;
+
+            while (low <= high)
+            {
+                int midIndex = low + (high - low) / 2;
+                if (numList[midIndex] == keyElement) return midIndex;
+
+                if (numList[midIndex] > keyElement)
+                {
+                    high = midIndex - 1;
+                }
+                else
+                {
+                    low = midIndex + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algorithm and Data Structure/SearchingAlgorithms/BinarySearch/Program.cs
@@ -0,0 +1,8 @@
+﻿using BinarySearch;
+
+int[] numList = new int[] { 1, 3, 5, 7 };
+int keyElement = 5;
+BinarySearcher searcher = new BinarySearcher();
+
+Console.WriteLine($"Position of element {keyElement} is {searcher.GetKeyElement(numList, keyElement)}");
+
